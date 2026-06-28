4f9809a045a76d96cd49af1665f6ad6985e86998
gachapingoo <waninocotype3@gmail.com>
2026-06-28 10:18:40 +0900

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="p-10 bg-blue-200 text-center">
       <h1 className="text-3xl font-bold text-blue-900">Tailwind OK!</h1>
       <p>aaa</p>
-      <input type="button" value="Click me" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" /> 
+      <form action="/next-page" method="post">
+      <input type="submit" value="次へ" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" /> 
     </div>
     
   )
